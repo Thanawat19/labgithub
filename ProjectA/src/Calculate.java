9ebf12181ad877182b77d7ad4a61fbc48e521f9e
Thanawat19 <teteeternal@gmail.com>
2021-09-12 18:43:54 +0700

    Calculate

--- ProjectA/src/Calculate.java
+++ ProjectA/src/Calculate.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
 
-public class Calculate {
-
-	public static void main(String[] args) {
+public class Calculate{
+	Employee employee;
+	public Employee getEmployee() {
+		return employee;
+	}
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	public Calculate(Employee employee) {
+		super();
+		this.employee = employee;
+	}
+	public static void calculate(String[] args) {
 		// TODO Auto-generated method stub
-
+		int commision;
+		if(employee.profit <= 50000) {
+			commision = profit+(profit*0.05);
+			
+			
+		}else if(profit >= 50001) {
+			commision = profit+(profit*0.10);
+		}
+		
 	}
 
 }
