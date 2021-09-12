c57b9e36854c0f87e6eae292c14b86eb7645bdb1
Roman2543 <ghostzrome2543@gmail.com>
2021-09-12 20:17:30 +0700

    create function calculate

--- ProjectA/src/Main.java
+++ ProjectA/src/Main.java
@@ -1,22 +1,52 @@
 import java.util.Scanner;
 public class Main {
 
-	public static void main(String[] args) {Scanner sc =new Scanner (System.in);
-		// TODO Auto-generated method stub
-		System.out.println("Input Your EmployeeID: ");
-		String employeeId= sc.nextLine();
-		System.out.println("Input Your Name: ");
-		String employeeName= sc.nextLine();
-		System.out.println("Input Your Salary: ");
-		int Salary= sc.nextInt();
-		Employee person1=new Employee(employeeId,employeeName,Salary);
-		System.out.println("Search Employee by ID: ");
-		employeeId= sc.nextLine();
-		if(person1.getEmployeeId()==employeeId) {
-			System.out.println("Input Your Profit: ");
-			int profit = sc.nextInt();
-			person1.setProfit(profit);
+	static double Calculate(double monthlysales,double salary){
+		double commission = 0;
+		if(monthlysales <= 50000) {
+			commission = salary+(monthlysales*0.05);
 		}
+		else if(monthlysales >= 50001) {
+			commission = salary+(monthlysales*0.10);
+		}
+		
+		return commission;
 	}
 
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner (System.in);
+		Scanner sc2 =new Scanner (System.in);
+		// TODO Auto-generated method stub
+		Boolean checkEmployee=false;
+		System.out.print("Input Your EmployeeID: ");
+		String employeeId= sc.nextLine();
+		System.out.print("Input Your Name: ");
+		String employeeName= sc.nextLine();
+		System.out.print("Input Your Salary: ");
+		int salary= sc.nextInt();		
+		System.out.println("__________________________________");
+		
+		
+		while (checkEmployee==false){
+			System.out.print("Input Your Id: ");
+			String employeeId2= sc2.nextLine();
+			System.out.println(employeeId);
+			System.out.println(employeeId2);
+//				if(employeeId2==employeeId) {
+					System.out.print("Input monthly sales : ");
+					int monthlysales = sc.nextInt();
+					double compen=Calculate(monthlysales,salary);
+					
+					checkEmployee=true;
+//				}
+//				else {
+//					System.out.println("Id Not Found");
+//				}
+			}
+	}
 }
+	
+
+
+
