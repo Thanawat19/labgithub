57d62266ae90b712b3715de7a0e6b3f6af4d1ee0
Thanawat19 <teteeternal@gmail.com>
2021-09-12 23:40:40 +0700

    release Project_B

--- ProjectA/src/Calculate.java
+++ ProjectA/src/Calculate.java
@@ -8,11 +8,11 @@ public void cal (Employee em){
     double commision=0;
     if (em.getmonthlysales()<= 50000){
         commision = em.getSalary()+(em.getmonthlysales()*0.05);    }
+	    if(commision < 12000) {
+	    	commision = 12000;
+	    }
     else if(em.getmonthlysales() >= 50001) {
         commision = em.getSalary()+(em.getmonthlysales()*0.10);
-    }
-    if(commision < 12000) {
-    	commision = 12000;
     }
     System.out.printf("%s get paid = %.2f Baht",em.getEmployeeName(),commision);
 }
