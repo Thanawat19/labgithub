b28817aa4b4573ef3bd39b7afda6c3887e3017cd
Roman2543 <ghostzrome2543@gmail.com>
2021-09-13 00:09:20 +0700

    release projectC

--- ProjectA/src/Calculate.java
+++ ProjectA/src/Calculate.java
@@ -6,10 +6,16 @@ public Calculate() {
 
 public void cal (Employee em){
     double totalsalary=0;
+    
     if (em.getmonthlysales()<= 50000){
-        totalsalary = em.getSalary()+(em.getmonthlysales()*0.05);    }
+        totalsalary = em.getSalary()+(em.getmonthlysales()*0.05);  
+	  }
+	    
     else if(em.getmonthlysales() >= 50001) {
         totalsalary = em.getSalary()+(em.getmonthlysales()*0.10);
+        if(em.getSalary() < 15000 && em.getmonthlysales()>100000) {
+        	totalsalary = totalsalary+5000;
+        }
     }
     System.out.printf("%s get paid = %.2f Baht",em.getEmployeeName(),totalsalary);
 }
